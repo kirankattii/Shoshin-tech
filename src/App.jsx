ab69b0cfee126dc8362f5a1d9d73d79b177aa675
kiran katti <kirankatti550@gmail.com>
2024-12-12 02:04:30 +0530

    updated the sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,16 @@ import styles from './app.module.css'
 import Dashboard from './Pages/Dashboard/Dashboard'
 import Navbar from './components/Navbar/Navbar'
 import Sidebar from './components/Sidebar/Sidebar'
-import { useState } from 'react'
 
 const App = () => {
-  const [showSidebar, setShowSidebar] = useState(false);
   return (
     <div>
       <div className={styles.appContainer}>
-        <Sidebar />
-        <div className={styles.navbar}>        <Navbar />
+        <div className={styles.sidebar}>
+          <Sidebar />
+        </div>
+        <div className={styles.navbar}>
+          <Navbar />
         </div>
       </div>
 
--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,6 +1,6 @@
 import { assets } from '../../assets/assets'
 import styles from './sidebar.module.css'
-import { Grid, UserPlus, Calendar, Users, Building, Headphones, Settings, X } from 'lucide-react';
+import { Grid, UserPlus, Calendar, Users, Building, Headphones, Settings } from 'lucide-react';
 import { menuData } from '../../assets/assets';
 import { motion } from 'framer-motion';
 
@@ -14,6 +14,7 @@ const iconMap = {
   Settings
 };
 
+// const isSmallScreen = window.innerWidth <= 1230;
 
 const Sidebar = () => {
   return (
@@ -31,13 +32,13 @@ const Sidebar = () => {
               {section.items.map((item, itemIndex) => {
                 const Icon = iconMap[item.icon]; // Dynamically map icon
                 return (
-                  <motion.li
+                  <li
                     key={itemIndex}
-                    initial={{ opacity: 0, x: -60 }} whileInView={{ opacity: 1, x: 0 }} transition={{ duration: 0.5, delay: itemIndex * 0.05 }}
+                    initial={{ scale: 0.5 }} whileInView={{ scale: 0 }} transition={{ duration: 0.5, delay: itemIndex * 0.05 }}
                   >
-                    <Icon size={18} />
+                    <Icon size={18} className={styles.icon} />
                     <span>{item.label}</span>
-                  </motion.li>
+                  </li>
                 );
               })}
             </ul>
